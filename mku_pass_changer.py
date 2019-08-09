02c109096f6b3f69db43b3f1765502218e010ec8
sir_lutta <38002486+LuttaKing@users.noreply.github.com>
2019-08-09 21:14:29 +0000

    Add files via upload

---
+++ mku_pass_changer.py
@@ -0,0 +1,36 @@
+# import the necessary modules...{bs4,re,requests}
+import requests,re
+from bs4 import BeautifulSoup
+
+#important links
+url="https://studentportal.mku.ac.ke/umis/studentportal/"
+fees_url='https://studentportal.mku.ac.ke/umis/studentportal/statement_detailed.php'
+pass_url='https://studentportal.mku.ac.ke/umis/studentportal/password_update.php'
+
+# student credentials
+userName= "dit/2019/44286"
+password="Luttason6"
+maybe=" Login "
+
+currentPassword="Luttason6"
+newPassword="Luttason9"
+confirmPassword='Luttason9'
+passwordHint='Awesomeness'
+changePassword='Change Password'
+
+login_payload = dict(regNo=userName,smisPass=password,smisLogon=maybe)
+
+pass_payload=dict(currentPassword=currentPassword,newPassword=newPassword,confirmPassword=confirmPassword,passwordHint=passwordHint,changePassword=changePassword)
+
+#start a session
+with requests.session() as c:
+
+    c.post(url,data=login_payload,headers={'Referer':'https://studentportal.mku.ac.ke/umis/studentportal/'})
+    c.post(pass_url,data=pass_payload,headers={'Referer':'https://studentportal.mku.ac.ke/umis/studentportal/'})
+
+
+    res = c.get(fees_url)
+    soup = BeautifulSoup(res.text,features="lxml")
+
+    student_name=soup.p.string
+    print(student_name)
